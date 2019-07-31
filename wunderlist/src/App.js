c93039976b78d0d67f0c918b6a8359a40109b30a
Randy Warrington <warrington.randy@outlook.com>
2019-07-30 20:17:58 -0400

    added reducers and updated the app.js and index.js file

--- wunderlist/src/App.js
+++ wunderlist/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddTodo from "./containers/AddTodo"
+import TodoList from "./containers/TodoList";
+import Footer from "./containers/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <AddTodo />
+    <TodoList />
+    <Footer />
+  </div>
+);
 
 export default App;
